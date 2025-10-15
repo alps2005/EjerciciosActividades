8d47d51b01d52ffb9ab55203f6a594c7b3f3bc06
Adrian Palma <adrianpalmasantana@gmail.com>
2025-10-14 19:11:26 -0500

    onCreateOptinosMenu to start the menu and onOptionItemSelected to choose the options

--- app/src/main/java/com/example/ejerciciosactividades/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciosactividades/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.ejerciciosactividades
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +12,25 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var id = item.itemId
+        if (id == R.id.menu_contador){
+            startActivity(Intent(this, ContadorActivity::class.java))
+        }else if (id == R.id.menu_conversor){
+            startActivity(Intent(this, ConversorUnidadesActivity::class.java))
+        }else{
+            startActivity(Intent(this, FormularioRegistroActivity::class.java))
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
